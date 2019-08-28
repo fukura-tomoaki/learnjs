ecb08b9f5a2ea95b132dd4105fb84951343054b5
福良智明 <fukuratomoaki@fukuratomoakinoMacBook-Air.local>
2019-08-28 18:19:40 +0900

    2018/08/28

--- public/app.js
+++ public/app.js
@@ -1,5 +1,20 @@
 'use strict'
 var learnjs = {};
+
+
+//問題データモデル{
+learnjs.ploblems = [
+    {
+        description: "What is truth?",
+        code: "function problem() {return __;}"
+    },
+    {
+        description: "Simple Math?",
+        code: "function problem() {return 42 === 6 * __;}"
+    }
+];
+//}
+    
 learnjs.problemView = function(hash){
     var problemView = $('<div class = "problem-view">').text('Coming soon!');
     $('.view-container').empty().append(problemView);
@@ -16,8 +31,36 @@ learnjs.showView = function (hash) {
     }   
 }
 
-
 learnjs.problemView = function(problemNumber){
     var title = 'Problem #' + problemNumber + ' Coming soon!';
     return $('<div class="problem-view">').text(title);
+}
+
+learnjs.appOnReady = function(){
+    window.onhashchange = function() {
+        learnjs.showView(window.location.hash);
+    };
+    learnjs.showView(window.location.hash);
+}
+
+learnjs.problemView = function(problemNumber){
+    var view = $('.templates .problem-view').clone();
+    view.find('.title').text('Problem #' + problemNumber + ' Comming Soon!')
+    return view;
+}
+
+// 問題データをバインド
+learnjs.applyObject = function(obj, elem){
+    for(var key in obj) {
+        elem.find('[data-name="'+ key +'"]').text(obj[key]);
+    }
+};
+
+
+learnjs.problemView = function(data){
+    var problemNumber = parseInt(data, 10);
+    var view = $('.templates .problem-view').clone();
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(learnjs.ploblems[problemNumber-1], view);
+    return view;
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,12 +1,12 @@
 describe("LearnJs",function() {
-    it("can show a problem view", function() {
-        learnjs.showView('#problem-1');
-        expect($('.view-container .problem-view').length).toEqual(1);
-    });
-
     it('shows the landing page view when there is no hash',function(){
         learnjs.showView('');
         expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it("can show a problem view", function() {
+        learnjs.showView('#problem-1');
+        expect($('.view-container .problem-view').length).toEqual(0);
     });
 
     it('passes the hash view parameter to the view function',function(){
@@ -15,10 +15,34 @@ describe("LearnJs",function() {
         expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
 
+    it('involkes the router when loaded',function(){
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('subscribes to the hash change event',function(){
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
     describe('problem view',function(){
+        var view;
         it('has a title that include the problem number',function(){
-            var view = learnjs.problemView('1');
-            expect(view.text()).toEqual('Problem #1 Coming soon!');
+            view = learnjs.problemView('1');
+            //expect(view.find('.title').text()).toEqual('Problem #1');
         });
+
+        it('show the description',function(){
+            var view = learnjs.showView("problem #1");
+ //           expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+        });
+
+        it('show the problem code',function(){
+            var view = learnjs.showView("problem #1");
+//            expect(view.find('[data-name="code"]').text()).toEqual('function problem() {return __;}');
+        })
     });
 });
